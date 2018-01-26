8078022ab420058a0543d04040f4c5a1da3bce8f
Ilya Bykov <il.byckoff2011@hotmail.com>
2018-01-27 01:32:33 +0300

    main communication with users

--- bot.py
+++ bot.py
@@ -9,19 +9,32 @@ import keyboards
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+
 def start_greeting(bot, update, chat_data):
     chat_data.clear()
     update.message.reply_text('Привет! Я помогу тебе быстро подобрать отличный отель с помощью сервиса booking.com.', reply_markup=keyboards.REMOVE_KB)
     update.message.reply_text('Если ты готов - просто напиши мне любое сообщение')
 
-def start_description(bot, update, chat_data):
-    bot.send_message(update.message.chat_id, text='Рад твоему сообщению!')
-    bot.send_message(update.message.chat_id, text='Как я уже говорил, я использую сервис booking.com, как наиболее надежный и удобный сервис.')
-    bot.send_message(update.message.chat_id, text='Все бронирования ты будешь делать на booking.com самостоятельно, а я отберу для тебя отели согласно твоим личным предпочтениям.')
-    bot.send_message(update.message.chat_id, text='Если ты не останавливаешься в хостелах – ты больше не увидишь их в своем списке отелей, но, если захочешь, я буду показывать тебе только хостелы.')
-    bot.send_message(update.message.chat_id, text='Я не буду просить дополнительную комиссию, все цены – согласно ценам на booking.com, обязательно проверь!')
-    bot.send_message(update.message.chat_id, text='Чтобы подобрать лучшие варианты, мне нужно задать тебе несколько вопросов. Давай попробуем?)',
-                     reply_markup=keyboards.YES_NO_KB)
+    chat_data['message_type'] = 'start_description'
+
+def text_message_handler(bot, update, chat_data):
+    if chat_data['message_type'] == 'start_description':
+        bot.send_message(update.message.chat_id,
+                         text='Рад твоему сообщению! \n'
+                              'Как я уже говорил, я использую сервис booking.com, как наиболее надежный и удобный сервис. \n'
+                              'Все бронирования ты будешь делать на booking.com самостоятельно, а я отберу для тебя отели согласно твоим личным предпочтениям. \n'
+                              'Если ты не останавливаешься в хостелах – ты больше не увидишь их в своем списке отелей, но, если захочешь, я буду показывать тебе только хостелы. \n'
+                              'Я не буду просить дополнительную комиссию, все цены – согласно ценам на booking.com, обязательно проверь! \n'
+                              'Чтобы подобрать лучшие варианты, мне нужно задать тебе несколько вопросов. Давай попробуем?)',
+                         reply_markup=keyboards.YES_NO_KB)
+        return
+
+    elif chat_data['message_type'] == 'go_travel':
+        bot.send_message(update.message.chat_id,
+                         text='Пожалуйста подождите, ищем город в базе данных...')
+
+        # Reading and using database of cities...
+
 
 def type_of_hotel(bot, update):
     query = update.callback_query
@@ -47,6 +60,7 @@ def num_stars(bot, update):
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
 
+
 def save_parameters(bot, update):
     query = update.callback_query
 
@@ -55,12 +69,13 @@ def save_parameters(bot, update):
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
 
-def go_travel(bot, update):
+def go_travel(bot, update, chat_data):
     query = update.callback_query
 
     bot.edit_message_text(text='Отлично! Куда и когда ты собираешься в следующий раз? Напиши мне город и дату.',
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
+    chat_data['message_type'] = 'go_travel'
 
 
 def back_to_main(bot, update):
@@ -87,17 +102,17 @@ if __name__ == '__main__':
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start_greeting, pass_chat_data=True))
+    dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_error_handler(error)
 
-    dispatcher.add_handler(MessageHandler(Filters.text, start_description, pass_chat_data=True))
+    dispatcher.add_handler(MessageHandler(Filters.text, text_message_handler, pass_chat_data=True))
+
     dispatcher.add_handler(CallbackQueryHandler(type_of_hotel, pattern='type_of_hotel'))
     dispatcher.add_handler(CallbackQueryHandler(back_to_main, pattern='back_to_main'))
     dispatcher.add_handler(CallbackQueryHandler(quality_of_hotel, pattern='(th1)|(th2)|(th3)|(th4)|(th5)|(th6)|(th7)|(th8)|(th9)|(th10)|(th11)|(th0)'))
     dispatcher.add_handler(CallbackQueryHandler(num_stars, pattern='(qh60)|(qh70)|(qh80)|(qh90)|(qh0)'))
     dispatcher.add_handler(CallbackQueryHandler(save_parameters, pattern='(ns1)|(ns2)|(ns3)|(ns4)|(ns5)|(ns0)'))
-    dispatcher.add_handler(CallbackQueryHandler(go_travel, pattern='go_travel'))
-
-    dispatcher.add_handler(CommandHandler('help', help))
-    dispatcher.add_error_handler(error)
+    dispatcher.add_handler(CallbackQueryHandler(go_travel, pattern='go_travel', pass_chat_data=True))
 
     updater.start_polling()
     updater.idle()
